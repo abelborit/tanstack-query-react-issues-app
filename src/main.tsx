b3c3d420de82823ac30858e374ac95dcc70e16bb
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-08-01 23:08:55 -0500

    [add] QueryClient & ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
 import { router } from './router';
 
+// Create a client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
