ec7966a7f25a98b127ea8040f614ffde155b46dd
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-08-03 21:43:58 -0500

    [add] VITE_GITHUB_TOKEN

--- src/api/githug.api.ts
+++ src/api/githug.api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const githubApi_AxiosInstance = axios.create({
   baseURL: 'https://aapi.github.com/repos/facebook/react',
   headers: {
-    // TODO: Api Key de GitHub para expandir la cuota de consumo de la api
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
   },
 });
